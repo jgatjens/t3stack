62daa4422c8ba958ac88a321dfd8871eea50dbe7
jgatjens <jgatjens@gmail.com>
2023-12-01 16:26:28 -0300

    add NEXT_PUBLIC_APP_URL to use VERCEL_URL

--- src/env.mjs
+++ src/env.mjs
@@ -43,7 +43,13 @@ export const env = createEnv({
    * `NEXT_PUBLIC_`.
    */
   client: {
-    NEXT_PUBLIC_APP_URL: z.string().min(1),
+    NEXT_PUBLIC_APP_URL: z.preprocess(
+      // This makes Vercel deployments not fail if you don't set NEXTAUTH_URL
+      // Since NextAuth.js automatically uses the VERCEL_URL if present.
+      (str) => process.env.VERCEL_URL ?? str,
+      // VERCEL_URL doesn't include `https` so it cant be validated as a URL
+      process.env.VERCEL ? z.string() : z.string().url()
+    ),
   },
 
   /**
